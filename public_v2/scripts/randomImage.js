68e2a80df9fe428f8e38cd660dcced45e38fd0a5
Damien Davis-Neff <me@damiendavisneff.com>
2024-06-16 16:27:57 -0400

    Swap main preview slot
    You can now preview images in the main slot by clicking the secondary previews. This will swap the images. Also added a support button.

--- public_v2/scripts/randomImage.js
+++ public_v2/scripts/randomImage.js
@@ -14,9 +14,17 @@ const thumbnailImages = [
     "/images/Thumbnails/Chapter 4/Chapter 4 FNCS 2.jpg",
     "/images/Thumbnails/Grand Theft Auto/Unstoppable.webp",
 ];
+const bannerImages = [
+    "/images/Banners/Fade/Twitter.jpg",
+    "/images/Banners/Spiro/Twitter.jpg",
+    "/images/Banners/TMoose/Twitter.jpg",
+    "/images/Banners/YaGami/Twitter.webp",
+]
 
 PickRandomImage(introductionImages,[document.getElementById("introductionImage")]);
 PickRandomImage(thumbnailImages,document.getElementsByClassName("preview-images-item-picker"));
+PickRandomImage(bannerImages,document.getElementsByClassName("preview-images-item-picker-banner"));
+
 
 function _PickRandomImage(images,source) {
     // OLD VERSION OF FUNCTION; ONLY WORKS WITH ONE SOURCE
@@ -65,4 +73,17 @@ function PickRandomImage(images, sources) {
         _images = __images;
 
     }
+}
+
+function OnImageClick(previewSlot,previewClicked) {
+    _previewSlot = previewSlot.src;
+    _previewClicked = previewClicked.src;
+    previewSlot.style.opacity = 0;
+    previewClicked.style.opacity = 0;
+    setTimeout(() => {
+        previewSlot.src = _previewClicked;
+        previewClicked.src = _previewSlot;
+        previewSlot.style.opacity = 1;
+        previewClicked.style.opacity = 1;
+    }, 125);
 }
